e90c368a9d7335185801e5403d4926a4c631cf23
Nicholas Wong <lion9200.world@gmail.com>
2023-09-11 11:48:58 +0800

    updated host ur;

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -5,7 +5,13 @@ export const signUp = createAsyncThunk(
     'auth/signUp',
     async (params) => {
         try {
-            const res = await axios.post("https://social-media-i-rport-backend-gjf3.vercel.app/auth/register", { ...params, role: "user" });
+            const res = await axios.post("https://i-report-backend-1h2vuyynq-admiral9200.vercel.app/auth/register", { ...params, role: "user" }, {
+                headers: {
+                    'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+                    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // Specify allowed HTTP methods
+                    'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Specify allowed headers
+                }
+            });
             return res.data;
         } catch(err) {
             throw err;
@@ -17,13 +23,7 @@ export const signIn = createAsyncThunk(
     'auth/signIn',
     async (params) => {
         try {
-            const res = await axios.post("https://social-media-i-rport-backend-gjf3.vercel.app/auth/login", params, {
-                headers: {
-                    'Access-Control-Allow-Origin': '*', // Allow requests from any origin
-                    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // Specify allowed HTTP methods
-                    'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Specify allowed headers
-                }
-            });
+            const res = await axios.post("https://i-report-backend-1h2vuyynq-admiral9200.vercel.app/auth/login", params);
             console.log(res.data)
             if(res.status == 200) {
                 await localStorage.setItem("token", res.data.token);
