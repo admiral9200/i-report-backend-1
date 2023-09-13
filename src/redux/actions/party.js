7770f30708ce28abfa593b80d090431f5b27b03a
Nicholas Wong <lion9200.world@gmail.com>
2023-09-13 10:21:42 +0800

    resolved hosting error

--- src/redux/actions/party.js
+++ src/redux/actions/party.js
@@ -5,7 +5,7 @@ export const getParties = createAsyncThunk(
     'party/getParties',
     async () => {
         try {
-            const res = await axios.get("https://i-report-backend-admiral.onrender.com//party/getParties");
+            const res = await axios.get("https://i-report-backend-admiral.onrender.com/party/getParties");
             return res.data;
         } catch(err) {
             throw err;
